f2d31be18ec021ac4e6fc14ffb8f702d11a52559
Kaijian Wu <47338831+kwu5@users.noreply.github.com>
2026-03-18 16:56:11 -0700

    Setup config.py

---
+++ src/ingestion/news_api.py
@@ -0,0 +1,20 @@
+import logging
+logger = logging.getLogger(__name__)
+
+class NewsAPIClient:
+    def __init__(self) -> None:
+        pass
+
+    def fetch_financial_news(days_back : int = 1) -> list[dict]:
+        
+        return None
+
+    
+        
+    
+    
+    
+    
+    
+    
+    
